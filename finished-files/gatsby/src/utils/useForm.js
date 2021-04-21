dbb1f6e6001788832c66e116e791b0aadc8f8584
AdamAnSubtractM <adam.l.knee@gmail.com>
2021-04-21 09:37:21 -0400

    Update useForm hook.

--- finished-files/gatsby/src/utils/useForm.js
+++ finished-files/gatsby/src/utils/useForm.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 export default function useForm(defaults) {
   const [values, setValues] = useState(defaults);
 
-  function updateValue(e) {
+  const updateValue = function(e) {
     // check if its a number and convert
     let { value } = e.target;
     if (e.target.type === 'number') {
